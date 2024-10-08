24d501ad48cfbab05cb3de245d6e1ef1573451d0
Sammi Vakka <sammylinkton@outlook.com>
2024-10-08 14:39:56 -0400

    baseing everything out

---
+++ steamdata.js
@@ -0,0 +1,35 @@
+//This script will grab my steam activity, then show what game im playing under my profile tab
+async function test() {
+    const apiUrl = 'http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=51990F7C324539A6EA20059D74BA68EB&steamids=76561198324195037';
+    const debug = document.getElementById('replaceme');
+    try {
+        const response = await fetch(apiUrl);
+        if (!response.ok) {
+            throw new Error(`No. ${response.status}`);
+        }
+
+        const json = await response.json();
+        console.log(json);
+
+        const player = json.response.players[0];
+
+        if(player.gameextrainfo) {
+            console.log(player.gameextrainfo)
+            debug.textContent = player.gameextrainfo
+
+        
+        } else {
+            console.log("no game")
+        }
+        
+
+
+
+
+    } catch (error) {
+        console.error(error.message);
+    }
+    const obj = JSON.parse(apiUrl);
+
+}
+test()
